968f69271220ce56c13339c353e3dd1b0eb4588e
neifiCastillo <neifi03128@gmail.com>
2026-02-01 21:30:23 -0400

    Ejercicio 14 Complete - TabControl view Label

---
+++ Ejercicio14/TabControl.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Ejercicio14
+{
+    partial class TabControl
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TabControl));
+            tabControl1 = new System.Windows.Forms.TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(544, 335);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(4, 37);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(536, 294);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Inicio";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(label2);
+            tabPage2.Location = new Point(4, 37);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(536, 294);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Información";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(label3);
+            tabPage3.Location = new Point(4, 37);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(536, 294);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Acerca de";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(109, 129);
+            label1.Name = "label1";
+            label1.Size = new Size(312, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Bienvenido a la pestaña de Inicio";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(79, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(384, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Aquí se muestra información del sistema";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(50, 112);
+            label3.Name = "label3";
+            label3.Size = new Size(414, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Aplicación creada en .NET 8 Windows Forms";
+            // 
+            // TabControl
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(562, 364);
+            Controls.Add(tabControl1);
+            Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 4, 4, 4);
+            MaximizeBox = false;
+            Name = "TabControl";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "TabControl";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label1;
+        private Label label2;
+        private TabPage tabPage3;
+        private Label label3;
+    }
+}
---
+++ Ejercicio14/TabControl.cs
@@ -0,0 +1,10 @@
+namespace Ejercicio14
+{
+    public partial class TabControl : Form
+    {
+        public TabControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
